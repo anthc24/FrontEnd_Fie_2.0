bd7e5cdd38658b826245a95049e39df248490263
Anthony Carhuancho Enciso <86864149+anthc24@users.noreply.github.com>
2024-05-30 01:10:22 -0500

    Login
    Anth

--- src/app/component/login/login-index/login-index.component.ts
+++ src/app/component/login/login-index/login-index.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-login-index',
   templateUrl: './login-index.component.html',
   styleUrls: ['./login-index.component.css']
 })
-export class LoginIndexComponent {
+export class LoginIndexComponent implements AfterViewInit {
+  @ViewChild('signUpButton') signUpButton!: ElementRef;
+  @ViewChild('signInButton') signInButton!: ElementRef;
+  @ViewChild('container') container!: ElementRef;
 
+  ngAfterViewInit(): void {
+    this.signUpButton.nativeElement.addEventListener('click', () => {
+      this.container.nativeElement.classList.add('right-panel-active');
+    });
+
+    this.signInButton.nativeElement.addEventListener('click', () => {
+      this.container.nativeElement.classList.remove('right-panel-active');
+    });
+  }
 }
